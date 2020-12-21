c3279c1b8d03b9f1de54b4fcb46dad73ae14b1b0
OculusRift03 <sehej.bakshi@yahoo.in>
2020-12-21 22:35:11 +0530

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAqH-bdlMXYkUMKWO_wTzZ8OF0A_0OFLYE",
+  authDomain: "clone-ad3c4.firebaseapp.com",
+  projectId: "clone-ad3c4",
+  storageBucket: "clone-ad3c4.appspot.com",
+  messagingSenderId: "941317159082",
+  appId: "1:941317159082:web:66f5395adb971d401178d3",
+  measurementId: "G-TP06PV3SH7"
+};

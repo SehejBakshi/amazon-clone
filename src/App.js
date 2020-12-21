54dc5332d5cdc4a2d874e754064ef73357537727
OculusRift03 <sehej.bakshi@yahoo.in>
2020-12-22 00:08:42 +0530

    Updated Home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout/">
+            <Header />
+            <p>Checkout</p>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

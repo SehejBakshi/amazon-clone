e2bc6c715561f4c42cd4690cace8317c00a7d680
OculusRift03 <sehej.bakshi@yahoo.in>
2021-01-29 22:54:37 +0530

    Added Payment Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
+import Payment from './Payment';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import {auth} from "./firebase";
 import {useStateValue} from "./StateProvider";
@@ -42,6 +43,10 @@ function App() {
           <Route path="/checkout/">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment/">
+            <Header />
+            <Payment />
           </Route>
           <Route path="/">
             <Header />
